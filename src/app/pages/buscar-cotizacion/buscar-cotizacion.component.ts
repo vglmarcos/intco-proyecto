d565da5795e54fa8a6df2f2f2812e0d14871829f
Numpy <vgl.marcos@gmail.com>
2021-05-13 01:39:55 -0500

    buscar cot

--- src/app/pages/buscar-cotizacion/buscar-cotizacion.component.ts
+++ src/app/pages/buscar-cotizacion/buscar-cotizacion.component.ts
@@ -1,15 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
+}
+
+/** Constants used to fill up our data base. */
+const COLORS: string[] = [
+  'maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple', 'fuchsia', 'lime', 'teal',
+  'aqua', 'blue', 'navy', 'black', 'gray'
+];
+const NAMES: string[] = [
+  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
+  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+];
 
 @Component({
   selector: 'app-buscar-cotizacion',
   templateUrl: './buscar-cotizacion.component.html',
-  styleUrls: ['./buscar-cotizacion.component.css']
+  styleUrls: ['./buscar-cotizacion.component.css'],
+  providers: [
+    { provide: MatPaginatorIntl, useValue: CustomPaginator() }  // Here
+  ]
 })
-export class BuscarCotizacionComponent implements OnInit {
+export class BuscarCotizacionComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['id', 'name', 'progress', 'color', 'acciones'];
+  dataSource: MatTableDataSource<UserData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor() { 
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
+  }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  onDelete(row: any) {
+    console.log(row);
+  }
+
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+  };
+}
+
+function CustomPaginator() {
+  const customPaginatorIntl = new MatPaginatorIntl();
+  customPaginatorIntl.itemsPerPageLabel = 'Registros por página:';
+  customPaginatorIntl.previousPageLabel = 'Página anterior'
+  customPaginatorIntl.nextPageLabel = 'Página siguiente';
+  customPaginatorIntl.getRangeLabel = (page: number, size: number, length: number): string => {
+    length = Math.max(length, 0);
+    const startIndex = page * size;
+    // If the start index exceeds the list length, do not try and fix the end index to the end.
+    const endIndex = startIndex < length ? Math.min(startIndex + size, length) : startIndex + page;
+    return `${startIndex + 1} - ${endIndex} de ${length} registros`;
+  };
+  return customPaginatorIntl;
 }
